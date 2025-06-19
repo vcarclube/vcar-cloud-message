07082fe3b44139ff93ca6286bfac537ee150c36b
Tekhno Develop <tekhnosoftdevelop@gmail.com>
2025-06-19 08:01:41 -0300

    updates

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,15 +1,26 @@
 const admin = require('firebase-admin');
-const path = require('path');
 
-// Caminho para o arquivo de credenciais
-const serviceAccountPath = path.join(__dirname, 'firebase-credential.json');
+require('dotenv').config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 // Inicializar Firebase Admin SDK
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountPath),
-    // Opcional: você pode especificar o projectId aqui também
-    // projectId: 'vcarclube-26a10'
+    credential: admin.credential.cert(serviceAccount),
+    projectId: serviceAccount.project_id
   });
 }
 
